60c97a93f090f1cbd909bdd6fdc7edb0c6ca73fe
sujeet kushwaha <sujeetkushwaha@0123.com>
2025-06-27 18:20:40 +0530

    ..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,18 @@ import { assets } from "../assets/assets";
 const Footer = () => {
   return (
     <div
-      className=" pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden pb-4
-    "
+      className=" pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden pb-4"
       id="Footer"
     >
       <div className=" container mx-auto flex flex-col md:flex-row justify-between items-start">
         <div className=" w-full md:w-1/3 mb-8 md:mb-0">
           <img src={assets.logo_dark} alt="logo" />
           <p className=" text-gray-400 mt-4">
-            Real estate is a property consisting of land and the buildings on
-            it, along with its natural
+            Real estate is a property consisting<br/> of land and the buildings on
+            it,<br/> along with its natural resources.
           </p>
         </div>
-        <div className=" w-full md:w1/5 mb-8 md:mb-0">
+        <div className=" w-full md:w-1/3 mb-8 md:mb-0 ">
           <h3 className=" text-white text-lg font-bold mb-4">Company</h3>
           <ul className=" flex flex-col gap-2 text-gray-400">
             <a href="#Header" className=" hover:text-white">
@@ -34,15 +33,24 @@ const Footer = () => {
         </div>
         <div className="w-full md:w-1/3 ">
           <h3 className=" text-white text-lg font-bold mb-4">
-            Subsscribe to our newsletter{" "}
+            Subscribe to our newsletter
           </h3>
           <p
             className="
              text-gray-400 mb-4 max-w-80 "
           >
-            The latest news, articals, and resources, sent to your inbox weekly.
+            The latest news, articles, and resources, sent to your inbox weekly.
           </p>
+          <div className="flex gap-2">
+            <input type="email" placeholder="Enter Your email"
+              className=" p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto
+            "/>
+            <button className=" py-2 px-4 rounded bg-blue-500 text-white cursor-pointer">Subscribe</button>
+          </div>
         </div>
+      </div>
+      <div className=" border-t border-gray-700 py-4 mt-10 text-center text-gray-500">
+        Copyright 2025 © SUJEETTX . All Right Reserved
       </div>
     </div>
   );
